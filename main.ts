7aa395110b8190f7cdb059910ed3b71db7c6f3f7
lrqmyxqnpm <152773153+lrqmyxqnpm@users.noreply.github.com>
2024-06-14 22:25:44 +0900

    更新 pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,11 @@
-
+let Y = 0
+let X = 0
+let S1 = game.createSprite(2, 2)
+basic.forever(function () {
+    X = S1.get(LedSpriteProperty.X)
+    Y = S1.get(LedSpriteProperty.Y)
+    S1.move(1)
+    led.plot(X, Y)
+    S1.ifOnEdgeBounce()
+    S1.turn(Direction.Right, 45)
+})
